1fae76cc4db787725f7fab2808d24ac50c48475c
Sipoufo Yvan <58191884+Sipoufo@users.noreply.github.com>
2022-04-21 00:49:40 +0100

    end more recipes part

--- src/components/home/moreRecipes.js
+++ src/components/home/moreRecipes.js
@@ -1,16 +1,18 @@
 // Modules
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 // Files
 import OneRecipe from './oneRecipe'
 
 const MoreRecipes = () => {
+    const {t} = useTranslation();
     return (
         <div className='flex w-full flex-col'>
             {/* Introduction part */}
             <div className='flex w-full flex-row'>
                 <div className='w-1/2 flex justify-start'>
-                    <h1 className='text-4xl font-bold'>Try this delicious recipe to make your day</h1>
+                    <h1 className='text-4xl font-bold'>{t('navbar.introductionMoreReipes')}</h1>
                 </div>
                 <div className='w-1/2 flex justify-start'>
                     <p className='text-sm'>Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </p>
